04b17fbb638baffefc14c6ccd3d144ebafab3ff4
liyusang1 <liyusang1@naver.com>
2021-03-31 01:10:28 +0900

    update c# script

---
+++ get all community.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        var client = new RestClient("https://liyusang1.site/community");
+        client.Timeout = -1;
+        var request = new RestRequest(Method.GET);
+
+        IRestResponse response = client.Execute(request);
+
+        //받아온 데이터를 json형태로 묶음
+        var jObject = JObject.Parse(response.Content);
+
+        //jObject.Count 는 서버로부터 넘어오는 list의 개수
+        for (int i = 0; i < jObject.Count; i++)
+        {
+            int contentId = (int)jObject["result"][i]["contentId"];
+            string contentName = jObject["result"][i]["contentName"].ToString();
+            string userNickName = jObject["result"][i]["userNickname"].ToString();
+            int replyCount = (int)jObject["result"][i]["replyCount"];
+            string createdAt = jObject["result"][i]["createdAt"].ToString();
+
+            Console.Write("글 작성자 :{0} , 글 제목 :{1} ,댓글 수 : {2}, 생성시기 : {3}", userNickName, contentName,replyCount,createdAt);
+            Console.WriteLine();
+        }
+        
+       
+    }
+}
+
---
+++ post community.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+
+        var client = new RestClient("https://liyusang1.site/community");
+        client.Timeout = -1;
+        var request = new RestRequest(Method.POST);
+        request.AddHeader("x-access-token", "");
+        request.AddHeader("Content-Type", "application/json");
+
+        string getContentName;
+        string getContent;
+
+        Console.Write("글 제목을 입력하세요 : ");
+        getContentName = Console.ReadLine();
+
+        Console.Write("글을 입력하세요 : ");
+        getContent = Console.ReadLine();
+
+        request.AddJsonBody(
+            new
+            {
+                contentName = getContentName,
+                content = getContent,
+            });
+
+        IRestResponse response = client.Execute(request);
+
+        //받아온 데이터를 json형태로 묶음
+        var jObject = JObject.Parse(response.Content);
+
+        //그걸 출력해 보자.
+        Console.WriteLine(response.Content);
+    }
+}
