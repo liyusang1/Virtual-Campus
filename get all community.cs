bcef4c7c45b0931d67ee92726853f737bb7a8a5d
liyusang1 <liyusang1@naver.com>
2021-04-08 02:03:57 +0900

    update get all community :pencil2:

--- get all community.cs
+++ get all community.cs
@@ -18,8 +18,8 @@ class Test
         //받아온 데이터를 json형태로 묶음
         var jObject = JObject.Parse(response.Content);
 
-        //jObject.Count 는 서버로부터 넘어오는 list의 개수
-        for (int i = 0; i < jObject.Count; i++)
+        int i = 0;
+        while (true)
         {
             int contentId = (int)jObject["result"][i]["contentId"];
             string contentName = jObject["result"][i]["contentName"].ToString();
@@ -27,11 +27,22 @@ class Test
             int replyCount = (int)jObject["result"][i]["replyCount"];
             string createdAt = jObject["result"][i]["createdAt"].ToString();
 
-            Console.Write("글 작성자 :{0} , 글 제목 :{1} ,댓글 수 : {2}, 생성시기 : {3}", userNickName, contentName,replyCount,createdAt);
+            Console.Write("글 작성자 :{0} , 글 제목 :{1} ,댓글 수 : {2}, 생성시기 : {3}", userNickName, contentName, replyCount, createdAt);
             Console.WriteLine();
+            i++;
+
+            if (jObject["result"][i].Next == null)
+            {
+                contentId = (int)jObject["result"][i]["contentId"];
+                contentName = jObject["result"][i]["contentName"].ToString();
+                userNickName = jObject["result"][i]["userNickname"].ToString();
+                replyCount = (int)jObject["result"][i]["replyCount"];
+                createdAt = jObject["result"][i]["createdAt"].ToString();
+
+                Console.Write("글 작성자 :{0} , 글 제목 :{1} ,댓글 수 : {2}, 생성시기 : {3}", userNickName, contentName, replyCount, createdAt);
+                Console.WriteLine();
+                break;
+            }
         }
-        
-       
     }
 }
-
