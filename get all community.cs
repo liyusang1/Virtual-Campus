8916cb0ad8e6f6e548e43070c80b3ad5ad9c1c9a
liyusang1 <liyusang1@naver.com>
2021-04-08 20:46:34 +0900

    update get all community

--- get all community.cs
+++ get all community.cs
@@ -18,8 +18,9 @@ class Test
         //받아온 데이터를 json형태로 묶음
         var jObject = JObject.Parse(response.Content);
 
-        int i = 0;
-        while (true)
+        int countContent = (int)jObject["countContent"];
+
+        for(int i=0; i<countContent; i++)
         {
             int contentId = (int)jObject["result"][i]["contentId"];
             string contentName = jObject["result"][i]["contentName"].ToString();
@@ -29,20 +30,6 @@ class Test
 
             Console.Write("글 작성자 :{0} , 글 제목 :{1} ,댓글 수 : {2}, 생성시기 : {3}", userNickName, contentName, replyCount, createdAt);
             Console.WriteLine();
-            i++;
-
-            if (jObject["result"][i].Next == null)
-            {
-                contentId = (int)jObject["result"][i]["contentId"];
-                contentName = jObject["result"][i]["contentName"].ToString();
-                userNickName = jObject["result"][i]["userNickname"].ToString();
-                replyCount = (int)jObject["result"][i]["replyCount"];
-                createdAt = jObject["result"][i]["createdAt"].ToString();
-
-                Console.Write("글 작성자 :{0} , 글 제목 :{1} ,댓글 수 : {2}, 생성시기 : {3}", userNickName, contentName, replyCount, createdAt);
-                Console.WriteLine();
-                break;
-            }
         }
     }
 }
