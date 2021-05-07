b6455f185f81adcfd1d202f6203a817c20f157b1
liyusang1 <liyusang1@naver.com>
2021-05-07 15:27:05 +0900

    add patch user inventory

---
+++ test C# code/patch user inventory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+class Test
+{
+    static void Main(string[] args)
+    {
+
+        var client = new RestClient("https://liyusang1.site/user-inventory");
+        client.Timeout = -1;
+        var request = new RestRequest(Method.PATCH);
+        request.AddHeader("x-access-token", "token");
+        request.AddHeader("Content-Type", "application/json");
+        request.AddJsonBody(
+         new
+         {
+             itemCode = "itemcode"
+         });
+
+
+        IRestResponse response = client.Execute(request);
+        var jObject = JObject.Parse(response.Content);
+
+        int code = (int)jObject["code"];
+
+        if (code == 1000)
+        {
+            
+        }
+
+        //예외
+        else
+        {
+
+        }
+    }
+}
