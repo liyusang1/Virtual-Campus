4c02f957dd6a061bf504df88f7ff8e104076f7b9
liyusang1 <liyusang1@naver.com>
2021-05-13 18:40:59 +0900

    add getUserCustomizing2

--- test C# code/get userCustomizing.cs
+++ test C# code/get userCustomizing.cs
@@ -14,7 +14,7 @@ class Test
         var client = new RestClient("https://liyusang1.site/user-customizing");
         client.Timeout = -1;
         var request = new RestRequest(Method.GET);
-        request.AddHeader("x-access-token", "token");
+        request.AddHeader("x-access-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInVzZXJOaWNrbmFtZSI6Iu2FjOyKpO2KuCIsImlhdCI6MTYxNjg2NjQ3NiwiZXhwIjoxNjQ4NDAyNDc2LCJzdWIiOiJ1c2VySW5mbyJ9.6U7rtn4F2Do_eBqJILH_Axr_CY9Q7_V2h-UxVMllQw8");
         IRestResponse response = client.Execute(request);
 
         var jObject = JObject.Parse(response.Content);
---
+++ test C# code/get userCustomizing2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+class Test
+{
+    static void Main(string[] args)
+    {
+
+        var client = new RestClient("https://liyusang1.site/user-customizing");
+        client.Timeout = -1;
+        var request = new RestRequest(Method.POST);
+        request.AddHeader("x-access-token", "token");
+        request.AddHeader("Content-Type", "application/json");
+
+        request.AddJsonBody(
+            new
+            {
+                hairR = "1",
+                hairG = "1",
+                hairB = "1",
+                eyeR = "1",
+                eyeG ="1",
+                eyeB = "1",
+                topR ="1",
+                topG ="1",
+                topB ="1",
+                bottomR ="1",
+                bottomG = "1",
+                bottomB = "1",
+                shoeR ="1",
+                shoeG ="1",
+                shoeB = "2"
+            });
+
+        IRestResponse response = client.Execute(request);
+    
+        var jObject = JObject.Parse(response.Content);
+
+        int code = (int)jObject["code"];
+
+        
+        if (code == 1000)
+        {
+            string message = jObject["message"].ToString();
+            Console.WriteLine(message);
+        }
+
+        //이미 생성되어 있는 경우 패스
+        else if (code == 1001)
+        {
+            string message = jObject["message"].ToString();
+            Console.WriteLine(message);
+        }
+
+    }
+}
