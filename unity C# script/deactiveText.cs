b6687e3d74aaf786f97802704c82b7ecc5c8fe47
liyusang1 <liyusang1@naver.com>
2021-06-04 23:10:57 +0900

    Add : furnitureChange

---
+++ unity C# script/deactiveText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deactiveText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.activeSelf == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("대화종료");
+                deactiveTextBox();
+            }
+        }
+    }
+
+    public void deactiveTextBox()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ unity C# script/furnitureChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class furnitureChange : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public Sprite fur_sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+       //sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeSprite()
+    {
+        sr.sprite = fur_sprite;
+    }
+}
