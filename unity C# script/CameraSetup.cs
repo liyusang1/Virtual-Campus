833953479ce6b7f42feb39bac77cca3b89d0fc4c
liyusang1 <liyusang1@naver.com>
2021-06-07 22:31:06 +0900

    add deactiveText

---
+++ unity C# script/CameraSetup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraSetup : MonoBehaviourPun
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (photonView.IsMine) 
+        {
+            CinemachineVirtualCamera followCam = FindObjectOfType<CinemachineVirtualCamera>();
+
+            followCam.Follow = transform;
+            followCam.LookAt = transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
