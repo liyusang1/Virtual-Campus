168aced094388f880a8a2399ebd31547f226d94d
liyusang1 <liyusang1@naver.com>
2021-06-07 22:34:13 +0900

    Add LobbyManager

---
+++ unity C# script/LobbyManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    public Text connectionInfoText;
+    public Button joinButton;
+    private readonly string gameVersion = "1";
+    public byte maxPlayer;
+    public string sceneName;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.Disconnect();
+        Screen.SetResolution(2560, 1440, true);
+
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+
+        joinButton.interactable = false;
+        connectionInfoText.text = "Connectiong To Master Server";
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        joinButton.interactable = true;
+        connectionInfoText.text = "Online : Connected to Master Server";
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        joinButton.interactable = false;
+        connectionInfoText.text = $"Offline : Connection Disabled {cause.ToString()} ";
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void Connect()
+    {
+        joinButton.interactable = false;
+
+        if (PhotonNetwork.IsConnected)
+        {
+            connectionInfoText.text = "Connecting to Random Room...";
+
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            connectionInfoText.text = $"Offline : Connection Disabled - Try Reconnecting";
+
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        connectionInfoText.text = "There is no empty room, Creating new Room";
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayer });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        connectionInfoText.text = "Connection with Room";
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+}
