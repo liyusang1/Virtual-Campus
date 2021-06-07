5963f1b56b4ea2318145fb510f187d91da2d5340
liyusang1 <liyusang1@naver.com>
2021-06-07 22:34:46 +0900

    Add Music

---
+++ unity C# script/Music.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Music : MonoBehaviour
+{
+    public Slider backVolume;
+    public AudioSource audio;
+
+    private float backVol = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backVol = PlayerPrefs.GetFloat("backvol", 1f);
+        backVolume.value = backVol;
+        audio.volume = backVolume.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SoundSlider();
+    }
+
+    public void SoundSlider() {
+        audio.volume = backVolume.value;
+
+        backVol = backVolume.value;
+        PlayerPrefs.SetFloat("backvol", backVol);
+    }
+}
