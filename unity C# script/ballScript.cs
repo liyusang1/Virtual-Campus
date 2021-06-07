7384993be2f6e1a37dced9b799484a71397de467
liyusang1 <liyusang1@naver.com>
2021-06-07 22:30:39 +0900

    Add ballScript

---
+++ unity C# script/ballScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballScript : MonoBehaviour
+{
+    public SoccerManager soccerManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        soccerManager = FindObjectOfType<SoccerManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "goalpost_1")
+        {
+            soccerManager.AteamScore++;
+            soccerManager.spawnBall();
+            Destroy(this.gameObject);
+        }
+
+        if (collision.name == "goalpost_2")
+        {
+            soccerManager.BteamScore++;
+            soccerManager.spawnBall();
+            Destroy(this.gameObject);
+        }
+    }
+}
