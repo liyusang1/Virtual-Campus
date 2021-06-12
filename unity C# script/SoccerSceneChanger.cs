89876e3e2051c3b7fe77d5af689ed8dcd0a1f5f2
liyusang1 <liyusang1@naver.com>
2021-06-12 15:50:59 +0900

    Add SoccerSceneChanger

---
+++ unity C# script/SoccerSceneChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoccerSceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToHome()
+    {
+        SceneManager.LoadScene("SignIn");
+    }
+
+    public void GoToSoccer()
+    {
+        SceneManager.LoadScene("SoccerLobby");
+    }
+}
