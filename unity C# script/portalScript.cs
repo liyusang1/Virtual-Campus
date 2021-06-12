0ad6f8be39abe2460a65b07cd7675691f3063fe3
liyusang1 <liyusang1@naver.com>
2021-06-12 15:48:58 +0900

    Add portalScript

---
+++ unity C# script/portalScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class portalScript : MonoBehaviour
+{
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            LoadToRoom();
+        }
+    }
+
+    public void LoadToRoom()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
