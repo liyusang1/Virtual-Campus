1360d388a0c3ab44733cdbf834ad027fd14b51d8
liyusang1 <liyusang1@naver.com>
2021-06-12 15:52:08 +0900

    Add Users

---
+++ unity C# script/User.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour
+{
+
+    public string username;
+    public string email;
+
+    public User(string username, string email)
+    {
+        this.username = username;
+        this.email = email;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
