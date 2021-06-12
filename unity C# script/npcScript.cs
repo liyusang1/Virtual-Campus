c6eb84da17d6336c04d58a0aa1bea0c04f7b5faa
liyusang1 <liyusang1@naver.com>
2021-06-12 15:47:51 +0900

    Add npcScrpit

---
+++ unity C# script/npcScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class npcScript : MonoBehaviour
+{
+    public GameObject talkButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("NPC 범위에 들어옴");
+            talkButton.SetActive(true);
+        }
+
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("NPC 범위에서 나감");
+            talkButton.SetActive(false);
+        }
+    }
+}
