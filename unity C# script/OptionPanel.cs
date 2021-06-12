0bc799b56844950f8b575ad61a61ce7eab6048c4
liyusang1 <liyusang1@naver.com>
2021-06-12 15:48:09 +0900

    Add OptionPanel

---
+++ unity C# script/OptionPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionPanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
